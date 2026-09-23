8d51635dde81b50b3ed532aec235e79df45d0cd1
Kera-ux-ui <faernika97@gmail.com>
2026-09-23 07:20:25 +0000

    nadeys complite

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Yakunina-router-lab">
+    <BrowserRouter basename="mobdev-router-lab-Yakunina">
       <App />
     </BrowserRouter>
   </React.StrictMode>
